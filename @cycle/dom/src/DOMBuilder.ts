13c2422b2563162cd575f96b7738db5afcdb25c6
Jan van Brügge <supermanitu@gmail.com>
2017-06-12 13:46:16 +0200

    Now this works

--- @cycle/dom/src/DOMBuilder.ts
+++ @cycle/dom/src/DOMBuilder.ts
@@ -1,32 +1,30 @@
 import { VNode } from './VNode';
 import {Signal} from 'ysignal';
-export function constructDOM(parent: Element, root : VNode | Signal<string>) : [VNode | undefined, any[]] { //TODO: Better textNode handling
-    if(typeof root === 'function') {
-        let textNode = document.createTextNode(root.next());
+
+export function constructDOM(parent: Element, root : VNode | Signal<string>) : any[] { //TODO: Return VNode
+    if(typeof root.init === 'function') {
+        const signal = root.init();
+        const next = signal.next();
+        let textNode = document.createTextNode(next.value);
         parent.appendChild(textNode);
-        const setter = () => { textNode.nodeValue = root.next(); };
+        const setter = () => { textNode.nodeValue = signal.next().value; };
         return [undefined, [setter]];
     }
     let element = document.createElement(root.tagName);
 
     const attributeSetters = handleAttributes(element, root.data.attrs);
-    const childrenResult = root.children.map(constructDOM.bind(null, element));
-    const childrenSetters = (childrenResult[1] as any[][]).reduce((a,b) => a.concat(b), []);
+    const childrenResult = root.children.map(e => constructDOM(element, e))
+    console.log(childrenResult);
+    const childrenSetters = (childrenResult as any).reduce((a,b) => a.concat(b), []);
 
     parent.appendChild(element);
 
-    return [
-        {
-            tagName: root.tagName,
-            data: { ...root.data, elm: element },
-            children: childrenResult[0]
-        },
-        attributeSetters.concat(childrenSetters)
-    ];
+    return attributeSetters.concat(childrenSetters);
 }
 
 function handleAttributes(element: Element, attrs: any): any[] {
     let setters = [];
+    if(attrs) {
     Object.keys(attrs).forEach(key => {
         const value = attrs[key];
         if(typeof value !== 'function') {
@@ -37,6 +35,7 @@ function handleAttributes(element: Element, attrs: any): any[] {
             setters.push(setter);
         }
     });
+    }
 
     return setters;
 }
---
+++ src/index2.ts
@@ -0,0 +1,58 @@
+import {Stream} from 'xstream';
+import {Signal} from 'ysignal';
+import {run} from '@cycle/run';
+import {makeDOMDriver2, VNode, DOMSource} from '@cycle/dom';
+
+type State = {
+  count: number;
+};
+
+type Sources = {
+  DOM: DOMSource;
+  state: StateSource<State>;
+  windowHeight: Signal<number>;
+};
+
+type Sinks = {
+  DOM: any;
+  state: Stream<Reducer<State>>;
+};
+
+function main(sources: Sources): Sinks {
+  const click$ = sources.DOM.select('.foo').events('click').mapTo(null);
+
+  const incrementReducer$ = click$.mapTo(function incrementReducer(
+    prevState: State
+  ): State {
+    return {
+      count: prevState.count + 1
+    };
+  });
+
+  const initialReducer$ = Stream.of(function initialReducer(): State {
+    return {count: 0};
+  });
+
+  const reducer$ = Stream.merge(initialReducer$, incrementReducer$);
+
+  const vdomS = Signal.combine(
+    sources.state.stateS,
+    sources.windowHeight
+  ).map(([state, height]) =>
+    div('.container', [
+      div('.height', 'Height: ' + height),
+      button('.foo', 'Count: ' + state.count),
+      div('.not', 'Not this')
+    ])
+  );
+
+  return {
+    DOM: vdomS,
+    state: reducer$
+  };
+}
+
+run(onionify<any, any, any>(main, 'state'), {
+  windowHeight: () => Signal.from(() => window.outerHeight),
+  DOM: makeDOMDriver('#main-container')
+});
