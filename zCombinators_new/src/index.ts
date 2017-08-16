bf1523510b06db42bc7dc07ced8682f8420657ad
Jan van Brügge <supermanitu@gmail.com>
2017-08-16 11:26:32 +0200

    Add startWith operator

--- zCombinators_new/src/index.ts
+++ zCombinators_new/src/index.ts
@@ -1,12 +1,30 @@
-import { IStream, Transformer, create } from '../../xstream_new';
-import { ISignal } from '../../ysignal_new';
+import xs, { IStream, Transformer } from '../../xstream_new';
+import ys, { ISignal, Signal } from '../../ysignal_new';
 
 export function sampleCombine<T>(...signals: ISignal<any>[]): Transformer<T, any[]> {
-    return stream => create<any[]>(observer => {
+    return stream => xs.create<any[]>(observer => {
         stream.subscribe({
             next: t => observer.next([t].concat(signals.map(s => s.next().value))),
             error: observer.error,
             complete: observer.complete
         });
     });
+}
+
+export function startWith<T>(value: T): (s: IStream<T>) => Signal<T> {
+    return stream => {
+        let currentValue: T = value;
+        stream.subscribe({
+            next: t => {
+                currentValue = t;
+            },
+            error: () => {},
+            complete: () => {}
+        });
+        return ys.create<T>({
+            next(): IteratorResult<T> {
+                return { value: currentValue, done: false };
+            }
+        })
+    };
 }
